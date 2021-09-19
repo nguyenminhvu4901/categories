2b01705a7804bebfbe68f3687c2ce19ee3703fc7
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-09-19 23:02:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Trang cate của Vũ</title>
+        <link rel ="stylesheet" href="as/style.css">
+    </head>    
+    <body>
+        <div class = "header">
+            <img src = "img/logo.png" alt="" style ="height:40px; float:left"></img>
+            <a href ="#">Home</a>
+            <a href ="#">Categories</a>
+            <a href ="#">Foods</a>
+            <a href ="#">Contact</a>
+        </div>
+
+        <div class ="menu">
+            <h2 style ="text-align:center">Explore Foods</h2>
+            <div class ="a1" style ="margin:12px">
+                <img src="img/pizza.jpg" alt="" style="height:300px; margin:6px 6px">
+                <img src="img/burger.jpg" alt="" style="height:300px; margin:6px 6px">
+                <img src="img/momo.jpg" alt="" style="height:300px;margin:6px 6px">
+            </div>
+            
+            <div>
+                <img src="img/pizza.jpg" alt="" style="height:300px; margin:6px 6px">
+                <img src="img/burger.jpg" alt="" style="height:300px; margin:6px 6px">
+                <img src="img/momo.jpg" alt="" style="height:300px;margin:6px 6px">
+            </div>
+
+            <div>
+                <img src="img/pizza.jpg" alt="" style="height:300px; margin:6px 6px">
+                <img src="img/burger.jpg" alt="" style="height:300px; margin:6px 6px">
+                <img src="img/momo.jpg" alt="" style="height:300px;margin:6px 6px">
+            </div>
+
+        </div>
+        
+        <div class ='logo'>
+            <img style ='height:50px' src="https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/241562310_2005700076255065_7948077867378444342_n.png?_nc_cat=106&ccb=1-5&_nc_sid=aee45a&_nc_ohc=hQr4MfNjthoAX-pLPcf&_nc_ht=scontent.xx&oh=5a100f5f0c3ebe5b55550a29f8f1e40c&oe=616CB5CF" alt=""></img>
+            <img  style ='height:50px' src ="https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/241474871_239067664821646_4658897847860453896_n.png?_nc_cat=107&ccb=1-5&_nc_sid=aee45a&_nc_ohc=Neu0Qo-7Q4sAX-d7vqj&_nc_ht=scontent.xx&oh=4e31b5b4faabcfef84849c75ec0fc9d9&oe=616D4E2B" alt=""></img>
+            <img  style ='height:50px' src = "https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/241397862_1459806631065639_2452520555971802737_n.png?_nc_cat=100&ccb=1-5&_nc_sid=aee45a&_nc_ohc=7T5NKyIzOZQAX_ouzvk&_nc_ht=scontent.xx&oh=61ae39adef785708676161b4e97f7a87&oe=616DFA53" alt=""></img>
+        </div>
+        <h4 style ='text-align: center; margin:10px 0 '>All rights reserved. Designed By <a href=# style='color:pink'>Nguyễn Minh Vũ</a></h4>
+
+
+
+
+
+
+
+
+
+
+
+
+    </body> 
